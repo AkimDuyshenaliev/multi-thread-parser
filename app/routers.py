5eab81594b663cb7cdf5beac67f076702234587d
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2023-03-23 13:33:00 +0300

    Can now parse a list of addresses

--- app/routers.py
+++ app/routers.py
@@ -11,24 +11,25 @@ router = APIRouter(
 
 
 @router.post('/address', status_code=201)
-def get_avito_product_link(address, threads=None):
+def get_avito_product_link(addresses: list, threads: int | None = None):
     main_file = 'app/static/stars_and_comments.csv'
 
     lock = Lock() # Initialize lock object
     proxies = proxy_generator()  # Get proxies
-    main_page = get_product_main_page(address=address)
-    num_of_threads = int(threads) if threads else 5
-    th = [ParsingWithProxy(
-        main_page=main_page,
-        page_num=i, 
-        step=num_of_threads, 
-        address=address,
-        main_file=main_file,
-        proxies=proxies,
-        lock=lock) for i in range(1, num_of_threads+1)]
+    for address in addresses:
+        main_page = get_product_main_page(address=address)
+        num_of_threads = int(threads) if threads else 5
+        th = [ParsingWithProxy(
+            main_page=main_page,
+            page_num=i, 
+            step=num_of_threads, 
+            address=address,
+            main_file=main_file,
+            proxies=proxies,
+            lock=lock) for i in range(1, num_of_threads+1)]
 
-    for proxy in th:
-        proxy.start()
+        for proxy in th:
+            proxy.start()
 
-    for proxy in th:
-        proxy.join()
+        for proxy in th:
+            proxy.join()
